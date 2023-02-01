a172a515faadbcecda22df2dac3a5edf381962d4
qcdbc <96817760+qcdbc@users.noreply.github.com>
2023-02-01 15:37:26 +0800

    implementation of pytorch lsbm

--- Code/steganography/lsb_matching.py
+++ Code/steganography/lsb_matching.py
@@ -1,6 +1,6 @@
-import torch
 import numpy as np
 from PIL import Image
+import torch
 from text import texts
 from utils.string_utils import string2bin, bin2string
 
@@ -50,6 +50,44 @@ class LSBMatching(BaseStego):
                         mask[img_idx, 0, ind, jnd] -= 1/ 256
 
         return tf.add(container, mask)
+
+    @staticmethod
+    def pt_encode(container):
+        """
+        LSB Matching algorithm (+-1 embedding)
+        :param container: pytorch tensor shape (batch_size, chan, width, height)
+        :param information: array with int bits
+        :param stego: name of image with hidden message
+        """
+        n, chan, width, height = tuple(container.shape)
+        info = np.random.randint(0, 2, (n, 2002))
+        mask = np.zeros(list(container.size()))
+
+        print("Num of images: %s" % n)
+        for img_idx in range(n):
+            for i, bit in enumerate(info[img_idx]):
+                ind, jnd = i // width, i - width * (i // width)
+
+                if ((container[img_idx, 0, ind, jnd] + 1) * 127.5).int() % 2 != bit:
+                    if np.random.randint(0, 2) == 0:
+                        mask[img_idx, 0, ind, jnd] += 1 / 256
+                    else:
+                        mask[img_idx, 0, ind, jnd] -= 1 / 256
+
+        return torch.add(container, torch.Tensor(mask))
+
+    # def lsb_embed(self, input, message, mask):
+    #     lsb = torch.IntTensor((input + 1) * 127.5) % 2
+    #
+    #     pm1_mask = torch.FloatTensor(tf.where(tf.equal(lsb, message),
+    #                                     tf.zeros_like(mask),
+    #                                     1 - 2 * mask)).detach()
+    #
+    #     # pm1_mask = tf.stop_gradient(pm1_mask)
+    #
+    #     embed = torch.add(input, pm1_mask / 127.5)
+    #
+    #     return embed, pm1_mask, lsb
 
     @staticmethod
     def encode(container, information, stego='stego.png'):
