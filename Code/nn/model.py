52c93ae3c3ee6bb41028d38fc00a1dc2dfe5994e
qcdbc <96817760+qcdbc@users.noreply.github.com>
2023-02-02 10:51:00 +0800

    Add files via upload

--- Code/nn/model.py
+++ Code/nn/model.py
@@ -3,98 +3,95 @@ import torch.nn as nn
 import torch.autograd
 
 
-class Discriminator(nn.Module):
-    def __init__(self, image_shape=(3, 64, 64), df_dim=64):
-        super(Discriminator, self).__init__()
-        self.in_channel = image_shape[0]
-        self.conv = nn.Sequential(
-            nn.Conv2d(self.in_channel, df_dim, kernel_size=(5, 5), stride=2),
-            nn.BatchNorm2d(df_dim),
-            nn.LeakyReLU(),
-            nn.Conv2d(df_dim, df_dim * 2, kernel_size=(5, 5), stride=2),
-            nn.BatchNorm2d(df_dim * 2),
-            nn.LeakyReLU(),
-            nn.Conv2d(df_dim * 2, df_dim * 4, kernel_size=(5, 5), stride=2),
-            nn.BatchNorm2d(df_dim * 4),
-            nn.LeakyReLU(),
-            nn.Conv2d(df_dim * 4, df_dim * 8, kernel_size=(5, 5), stride=2),
-            nn.BatchNorm2d(df_dim * 8),
-            nn.LeakyReLU(),
-        )
-        self.fcn = nn.Sequential(
-            nn.Linear(df_dim * 8, 1),
-            )
-        
-    def forward(self, img):
-        # input shape: batch_size * length * width * channel -->  64 x 64 x 64 x 3
-        batch_size = img.shape[0]
-        out = self.conv(img).view(batch_size, -1)  # batch_size * (df_dim * 8)
-        out = self.fcn(out)
-        print(out.shape)
-        
-        return out
-
-
 class Steganalyser(nn.Module):
     def __init__(self, image_shape=(3, 64, 64), df_dim=64):
         super(Steganalyser, self).__init__()
         self.in_channel = image_shape[0]
         self.conv = nn.Sequential(
-            nn.Conv2d(self.in_channel, df_dim, kernel_size=(5, 5), stride=2),
-            nn.BatchNorm2d(df_dim),
-            nn.LeakyReLU(),
-            nn.Conv2d(df_dim, df_dim * 2, kernel_size=(5, 5), stride=2),
-            nn.BatchNorm2d(df_dim * 2),
-            nn.LeakyReLU(),
-            nn.Conv2d(df_dim * 2, df_dim * 4, kernel_size=(5, 5), stride=2),
-            nn.BatchNorm2d(df_dim * 4),
-            nn.LeakyReLU(),
-            nn.Conv2d(df_dim * 4, df_dim * 8, kernel_size=(5, 5), stride=2),
-            nn.BatchNorm2d(df_dim * 8),
-            nn.LeakyReLU(),
-        )
-        self.fcn = nn.Sequential(
-            nn.Linear(df_dim * 8, 1),
+            nn.Conv2d(3, df_dim, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(df_dim),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(df_dim, df_dim * 2, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(df_dim * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(df_dim * 2, df_dim * 4, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(df_dim * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(df_dim * 4, df_dim * 8, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(df_dim * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(df_dim * 8, 1, 4, 1, 0, bias=False),
+            # nn.Sigmoid(),
         )
 
-    def forward(self, img):
-        # input shape: batch_size * length * width * channel -->  64 x 64 x 64 x 3
-        batch_size = img.shape[0]
-        out = self.conv(img).view(batch_size, -1)  # batch_size * (df_dim * 8)
-        out = self.fcn(out)
-        print(out.shape)
+    def forward(self, inputs):
+        """
+        :param inputs: batch * c_channel * image_size * image_size --> 64 x 3 x 64 x 64
+        :return: batch * 1
+        """
+        out = self.conv(inputs)
+        return out.view(-1, 1)
 
-        return out
+
+class Discriminator(nn.Module):
+    def __init__(self, image_shape=(3, 64, 64), df_dim=64):
+        super(Discriminator, self).__init__()
+        self.in_channel = image_shape[0]
+        self.conv = nn.Sequential(
+            nn.Conv2d(3, df_dim, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(df_dim),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(df_dim, df_dim * 2, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(df_dim * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(df_dim * 2, df_dim * 4, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(df_dim * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(df_dim * 4, df_dim * 8, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(df_dim * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(df_dim * 8, 1, 4, 1, 0, bias=False),
+            # nn.Sigmoid(),
+        )
+
+    def forward(self, inputs):
+        """
+        :param inputs: batch * c_channel * image_size * image_size --> 64 x 3 x 64 x 64
+        :return: batch * 1
+        """
+        out = self.conv(inputs)
+        return out.view(-1, 1)
 
 
 class Generator(nn.Module):
-    def __init__(self, z_dim=100, gf_dim=64, img_shape=(3, 64, 64)):
+    def __init__(self, img_shape=(3, 64, 64), z_dim=100, gf_dim=64):
         super(Generator, self).__init__()
-        self.c_dim = img_shape[0]
-        self.gf_dim = gf_dim
-
-        self.linear = nn.Linear(z_dim, gf_dim * 8 * 4 * 4)
-
-        self.trans_conv = nn.Sequential(
-            nn.BatchNorm2d(self.gf_dim * 8),
-            nn.ReLU(),
-            nn.ConvTranspose2d(gf_dim * 8, gf_dim * 4, kernel_size=3, stride=2, padding=1, output_padding=1),
+        self.c_chan = img_shape[0]
+        self.conv = nn.Sequential(
+            nn.ConvTranspose2d(z_dim, gf_dim * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(gf_dim * 8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(gf_dim * 8, gf_dim * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(gf_dim * 4),
-            nn.ReLU(),
-            nn.ConvTranspose2d(gf_dim * 4, gf_dim * 2, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(gf_dim * 4, gf_dim * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(gf_dim * 2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(gf_dim * 2, gf_dim, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(gf_dim * 2, gf_dim, 4, 2, 1, bias=False),
             nn.BatchNorm2d(gf_dim),
-            nn.ReLU(),
-            nn.ConvTranspose2d(gf_dim, self.c_dim, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(gf_dim, self.c_chan, 4, 2, 1, bias=False),
             nn.Tanh()
         )
 
-    def forward(self, noise):
-        img = self.linear(noise).view(-1, self.gf_dim * 8, 4, 4)
-        out = self.trans_conv(img)
-
+    def forward(self, inputs):
+        """
+        :param inputs: batch * z_dim --> 64 x 100
+        :return: batch * c_channel * img_size * image_size --> 64 x 3 x 64 x 64
+        """
+        out = self.conv(inputs)
         return out
 
 
